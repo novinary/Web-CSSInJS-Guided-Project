4522aa64e493a1a462adb5a7bfcfd722bdb627d8
novinary <novina.dev@gmail.com>
2019-02-28 07:42:31 +0000

    Worked on the styling in JS

--- src/components/Playground.jsx
+++ src/components/Playground.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
+const getStyle = () => ({
+  header: {
+    padding: '10px',
+    marginTop: '10px',
+    border: `4px solid black`
+  },
+
+  nav: {
+    border: `5px solid red`,
+  },
+});
 
 export default function Playground() {
+  // didn't invoke it here earlier
+  let styles = getStyle();
   return (
-    <div>
+    <div style={styles.header}>
       This is the playground component
+      <div style={styles.nav}>Child's playground component</div>
     </div>
   );
 }
